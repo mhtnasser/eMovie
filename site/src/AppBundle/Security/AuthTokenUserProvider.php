9c1427eed5875869203e2e874d73e49aebc8a226
nasser <nasrimht24@gmail.com>
2018-08-08 11:37:29 +0200

    config using a token

---
+++ site/src/AppBundle/Security/AuthTokenUserProvider.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nasri
+ * Date: 30/07/2018
+ * Time: 11:51
+ */
+
+namespace AppBundle\Security;
+
+
+use Doctrine\ORM\EntityRepository;
+use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
+use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+
+class AuthTokenUserProvider implements UserProviderInterface
+{
+
+    protected $authTokenRepository;
+    protected $userRepository;
+
+    public function __construct(EntityRepository $authTokenRepository, EntityRepository $userRepository)
+    {
+        $this->authTokenRepository = $authTokenRepository;
+        $this->userRepository = $userRepository;
+    }
+
+    public function getAuthToken($authTokenHeader)
+    {
+        return $this->authTokenRepository->findOneByValue($authTokenHeader);
+    }
+
+    /**
+     * Loads the user for the given username.
+     *
+     * This method must throw UsernameNotFoundException if the user is not
+     * found.
+     *
+     * @param string $email
+     * @return UserInterface
+     * @internal param string $username The username
+     *
+     */
+    public function loadUserByUsername($email)
+    {
+        return $this->userRepository->findByEmail($email);
+    }
+
+    /**
+     * Refreshes the user.
+     *
+     * It is up to the implementation to decide if the user data should be
+     * totally reloaded (e.g. from the database), or if the UserInterface
+     * object can just be merged into some internal array of users / identity
+     * map.
+     *
+     * @param UserInterface $user
+     * @return UserInterface
+     */
+    public function refreshUser(UserInterface $user)
+    {
+        throw new UnsupportedUserException();
+    }
+
+    /**
+     * Whether this provider supports the given user class.
+     *
+     * @param string $class
+     *
+     * @return bool
+     */
+    public function supportsClass($class)
+    {
+        return 'AppBundle\Entity\User' === $class;
+    }
+}
